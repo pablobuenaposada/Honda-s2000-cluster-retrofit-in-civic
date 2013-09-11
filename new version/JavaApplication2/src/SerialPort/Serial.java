cda55b2d522c8570c01a828ec05d6f0ba9a2a267
Pablo <pablo.buenaposada@gmail.com>
2013-09-11 02:15:58 +0200

    first commit for the new version

---
+++ new version/JavaApplication2/src/SerialPort/Serial.java
@@ -0,0 +1,76 @@
+package SerialPort;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+public class Serial {
+	
+    private final static String usbPort = "/dev/tty.usbserial";
+    private final static String appName = "S2000 Cluster setup";
+    private final static int TIMEOUT=1500;
+    private static CommPortIdentifier portId;    
+    private static SerialPort serialPort;
+    private static OutputStream outputStream;
+    private static InputStream inputStream;    
+    
+    
+    /*public static void main (final String[] args) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException, InterruptedException {
+    	configPort(usbPort,appName);
+    	write("a/n");
+        Thread.sleep(500);
+        System.out.println(read());
+        closePort();
+    }*/
+    
+    public void closePort() throws IOException{
+    	outputStream.close();
+        serialPort.removeEventListener();
+        serialPort.close();        	
+    }
+    
+    public void configPort(String usbPort,String appName) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException{
+    	portId = CommPortIdentifier.getPortIdentifier(usbPort);
+
+        serialPort = (SerialPort)portId.open(appName, 2000);                  
+        serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+
+        outputStream = serialPort.getOutputStream();
+        inputStream = serialPort.getInputStream();
+    }
+    
+    public void write(String data) throws IOException{
+    	outputStream.write(data.getBytes());
+    }
+    
+    public String read() throws IOException{    	
+    	String received="";
+    	int bytesRead=0;
+    	byte[] buffer = new byte[100];
+	long timeoutTime = System.currentTimeMillis()+TIMEOUT;
+    			
+    	while (!received.contains(Character.toString('\n'))){				
+            if (inputStream.available() > 0){
+                bytesRead = inputStream.read(buffer);
+		received = received + new String(buffer).substring(0, bytesRead);
+            }
+            else{
+		if (System.currentTimeMillis() > timeoutTime){						
+                    return null;
+		}				
+            }
+        }				
+        return received;
+    }
+    
+    
+
+
+}
---
+++ new version/sketch_sep07a/sketch_sep07a.ino
@@ -0,0 +1,81 @@
+#include <TimerOne.h>
+#include <EEPROM.h>
+
+#define factorAddress 1
+int vssOut = 9;
+int vssIn = 7;
+double scale=0;
+int factor; 
+unsigned long durationHigh=0; 
+unsigned long durationLow=0;
+unsigned long period=0;
+unsigned long duty=0;
+String inputString = "";
+
+void setup(){
+  pinMode(vssOut, OUTPUT); //sets the pin as output
+  pinMode(vssIn, INPUT); //sets the pin as input
+  
+  factor = EEPROM.read(factorAddress);
+  inputString.reserve(200);
+  Serial.begin(9600);  
+  Timer1.initialize(); //initializing the Timer1, so we can use pwm function
+}
+ 
+void loop(){ 
+  
+      //reading pulse from vss
+      durationHigh = pulseIn(vssIn,HIGH);
+      durationLow = pulseIn(vssIn,LOW);
+      //divide the pulse to get it "factor" times faster (37 in our case)
+      durationHigh = durationHigh/factor;
+      durationLow = durationLow/factor;
+      //calculing period and duty of the new pulse
+      period=durationHigh+durationLow;
+      scale = 1024/period; //10bit duty in Timer1, range [0,1023]
+      if (scale < 1){ //this is because with low speeds we get duty=0,xx... and this make the pulse all time low (0v)
+        scale=1; //fixing this with the minimum reasonable value (1)
+      }
+      duty=scale*durationHigh;
+      Timer1.pwm(vssOut,duty,period); //we set the new custom pwm on vssOut pin
+}
+
+void serialEvent() {
+  boolean stringComplete = false;
+  inputString="";  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    } 
+  
+  }
+  
+  if (stringComplete){
+    if (inputString == "VSSREAD\n"){
+      Serial.print("VSSFACTOR");
+      Serial.println(EEPROM.read(factorAddress));       
+    }
+    else if(inputString.substring(0,6) == "VSSSET"){
+      EEPROM.write(factorAddress,inputString.substring(6,inputString.length()-1).toInt());  
+      Serial.println("VSSOK");
+    }
+    else{
+      Serial.println(inputString);
+    }
+  }
+  
+  
+}
+  
+  
+  
+  
+  
+  
+
